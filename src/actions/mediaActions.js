ffd8cef2c82356125f4c7fd1869f1c27c1b651bc
Marty McLaughlin <martymclaugh@gmail.com>
2017-03-07 16:15:22 -0800

    add media actions

--- src/actions/mediaActions.js
+++ src/actions/mediaActions.js
@@ -1,1 +1,16 @@
-3g
+import * as types from '../constants/actionTypes';
+
+export const selectImageAction = image => ({
+  type: types.SELECTED_IMAGE,
+  image
+})
+
+export const selectVideoAction = video => ({
+  type: types.SELECTED_VIDEO,
+  video
+});
+
+export const searchMediaAction = payload => ({
+  type: types.SEARCH_MEDIA_REQUEST,
+  payload
+})g
